0db42cbb1967447b5f74e26af5320e9a9429d72b
Markus Reiter <me@reitermark.us>
2021-06-20 01:34:11 +0200

    Add initial Opt4J configuration for bee colony optimizer.

--- src/main/java/at/uibk/dps/optfund/tsp/beecolony/BeeColonyOptimizer.java
+++ src/main/java/at/uibk/dps/optfund/tsp/beecolony/BeeColonyOptimizer.java
@@ -3,14 +3,38 @@ package at.uibk.dps.optfund.tsp.beecolony;
 import java.util.*;
 import java.util.stream.*;
 
-class BeeColonyOptimizer {
-  public static void main(String[] args) {
-    var optimizer = new BeeColonyOptimizer();
+import com.google.inject.*;
+import org.opt4j.core.optimizer.*;
+import org.opt4j.core.start.*;
 
-    optimizer.optimize(13, 4, new double[] {0, 0, 0, 0}, new double[] {1, 1, 1, 1});
+class BeeColonyOptimizer implements IterativeOptimizer {
+  private final Population population;
+
+  protected final int populationSize;
+  protected final int n;
+  protected final double lowerBound;
+  protected final double upperBound;
+
+  @Inject
+  public BeeColonyOptimizer(
+    Population population,
+    @Constant(value = "populationSize", namespace = BeeColonyOptimizer.class) int populationSize,
+    @Constant(value = "n", namespace = BeeColonyOptimizer.class) int n,
+    @Constant(value = "lowerBound", namespace = BeeColonyOptimizer.class) double lowerBound,
+    @Constant(value = "upperBound", namespace = BeeColonyOptimizer.class) double upperBound
+  ) {
+    this.population = population;
+    this.populationSize = populationSize;
+    this.n = n;
+    this.lowerBound = lowerBound;
+    this.upperBound = upperBound;
   }
 
-  BeeColonyOptimizer() {}
+  public static void main(String[] args) {
+    // var optimizer = new BeeColonyOptimizer();
+
+    // optimizer.optimize(13, 4, new double[] {0, 0, 0, 0}, new double[] {1, 1, 1, 1});
+  }
 
   void optimize(int populationSize, int n, double[] lowerBounds, double[] upperBounds) {
     var foodSources = IntStream.range(0, populationSize)
@@ -25,5 +49,15 @@ class BeeColonyOptimizer {
     var newSource = foodSources.get(0).neighbor(phi, foodSources.get(1));
 
     System.out.println(newSource);
+  }
+
+  @Override
+  public void initialize() throws TerminationException {
+    System.out.println("TEST initialize");
+  }
+
+  @Override
+  public void next() throws TerminationException {
+    System.out.println("TEST next");
   }
 }
